118023e32d649262df4e2f76bd53d2ae02372693
arkad <arkad@DESKTOP-JQ7QI7O>
2023-10-04 15:23:50 +0200

    Zadanie lekcja 4.

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Anna";
+char sex = 'K';
+var age = 35;
+
+if (age > 30 && sex =='K')
+{
+    if(name == "Anna" && age == 35)
+    {
+        Console.WriteLine("Hello Anna");
+    }
+    else
+    {
+        Console.WriteLine($"You are not Anna age: {age}" );   
+    }
+}
+else
+{
+    Console.WriteLine("Hello stranger");
+}
